33371b0cbd12729b0de035d1aa02d454499e30c6
Lipeeeeeee <favasconcelos09@gmail.com>
2024-04-04 20:20:20 +0000

    alterações para o segundo trabalho

--- mysite/enquetes/admin.py
+++ mysite/enquetes/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Pergunta, Alternativa
 
-# Register your models here.
+admin.site.register(Pergunta)
+admin.site.register(Alternativa)
--- mysite/enquetes/views.py
+++ mysite/enquetes/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Pergunta, Alternativa
+
 
 def index(request):
-    return HttpResponse("<h1>Disciplina: DS Web<br>Semestre: 2024.1<br>Matrícula: 20231014040027<br>Nome: Felipe Alves de Vasconcelos</h1>")
+    lista = Pergunta.objects.order_by("-data_pub")
+    return render(request, "enquetes/index.html", {"lista_enquetes": lista})
+
+def detalhes(request, pergunta_id):
+    pergunta = get_object_or_404(Pergunta, pk = pergunta_id)
+    return render(request, "enquetes/detalhes.html", {"enquete": pergunta})
+
+def votacao(request, pergunta_id):
+    pergunta = get_object_or_404(Pergunta, pk = pergunta_id)
+    try:
+        alternativa_id = request.POST["escolha"]
+        alternativa = pergunta.alternativa_set.get(pk = alternativa_id)
+    except (KeyError, Alternativa.DoesNotExist):
+        contexto = {"enquete": pergunta, "error": "Você precisa selecionar uma alternativa"}
+        return render(request, "enquetes/detalhes.html", contexto)
+    else:
+        alternativa.votos += 1
+        alternativa.save()
+        return HttpResponseRedirect(reverse("enquetes:resultado", args = (pergunta.id, )))
+
+def resultado(request, pergunta_id):
+    pergunta = get_object_or_404(Pergunta, pk = pergunta_id)
+    return render(request, "enquetes/resultado.html", {"enquete": pergunta})
