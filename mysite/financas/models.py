2c18388c0319c65dfc5ad1b9f622e757b7b021a3
Felipe Alves de Vasconcelos <20231014040027@ifrn.local>
2024-08-23 10:49:13 -0300

    arrumando a ordenação

--- mysite/financas/models.py
+++ mysite/financas/models.py
@@ -10,7 +10,7 @@ class Usuario(models.Model):
 
 class Balancete(models.Model):
     nome = models.CharField("Nome do balancete", max_length=30, unique=True)
-    data = models.DateField("Data de criação do balancete", auto_now_add=True)
+    data = models.DateTimeField("Data de criação do balancete", auto_now_add=True)
     saldo = models.FloatField("Saldo total do balancete", default=0)
     user = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     def __str__(self):
