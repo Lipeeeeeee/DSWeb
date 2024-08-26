17856f391c3d5ed7064b4428132674b0e1e37976
Lipeeeeeee <favasconcelos09@gmail.com>
2024-08-26 17:54:32 -0300

    formatação dos arquivos py para as convenções

--- mysite/financas/urls.py
+++ mysite/financas/urls.py
@@ -7,11 +7,17 @@ urlpatterns = [
     path("auth/", views.UserView.as_view(), name="authenticate"),
     path("logout/", views.UserView.logout, name="logout"),
     path("balancete/", views.BalanceteView.post, name="balancete"),
-    path("balancete/<int:pk>/", views.BalanceteView.get_balancete, name="get_balancete"),
+    path(
+        "balancete/<int:pk>/", views.BalanceteView.get_balancete, name="get_balancete"
+    ),
     path("inicio/", views.BalanceteView.as_view(), name="index"),
     path("receitas/<int:pk>/", views.ReceitaView.as_view(), name="receita"),
-    path("receitas/<int:pk>/adicionar/", views.ReceitaView.post, name="adicionar_receita"),
+    path(
+        "receitas/<int:pk>/adicionar/", views.ReceitaView.post, name="adicionar_receita"
+    ),
     path("despesas/<int:pk>/", views.DespesaView.as_view(), name="despesa"),
-    path("despesas/<int:pk>/adicionar/", views.DespesaView.post, name="adicionar_despesa"),
-    path("pesquisar/", views.TransacaoView.as_view(), name="transacao")
+    path(
+        "despesas/<int:pk>/adicionar/", views.DespesaView.post, name="adicionar_despesa"
+    ),
+    path("pesquisar/", views.TransacaoView.as_view(), name="transacao"),
 ]
--- mysite/financas/views.py
+++ mysite/financas/views.py
@@ -4,89 +4,123 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from .models import Balancete, Transacao, Receita, Despesa
 
+
 class BalanceteView(View):
     def get(self, request, *args, **kwargs):
         balancetes = Balancete.objects.all()
         return render(request, "financas/index.html", {"balancetes": balancetes})
-    
+
     def post(request, *args, **kwargs):
         nome = request.POST["nome"]
         try:
             Balancete.objects.create(nome=nome, user=request.user)
         except:
-            return render(request, "financas/index.html", {"feedback": "Não foi possível criar balancete!"})
+            return render(
+                request,
+                "financas/index.html",
+                {"feedback": "Não foi possível criar balancete!"},
+            )
         return redirect("financas:index")
-    
+
     def get_balancete(request, **kwargs):
         balancete = Balancete.objects.get(pk=kwargs["pk"])
         return render(request, "financas/balancete.html", {"balancete": balancete})
+
 
 class TransacaoView(View):
     def get(self, request, *args, **kwargs):
         nome_transacao = request.GET["pesquisa"]
         try:
-            transacoes = Transacao.objects.filter(nome__contains=nome_transacao, balancete__user=request.user)
+            transacoes = Transacao.objects.filter(
+                nome__contains=nome_transacao, balancete__user=request.user
+            )
         except:
-            return redirect('financas:index')
-        return render(request, "financas/transacoes.html", {"transacoes": transacoes, "pesquisa": nome_transacao})
+            return redirect("financas:index")
+        return render(
+            request,
+            "financas/transacoes.html",
+            {"transacoes": transacoes, "pesquisa": nome_transacao},
+        )
+
 
 class ReceitaView(View):
     def get(self, request, *args, **kwargs):
         balancete = Balancete.objects.get(pk=kwargs["pk"])
-        return render(request, 'financas/receita.html', {'balancete': balancete})
+        return render(request, "financas/receita.html", {"balancete": balancete})
 
     def post(request, **kwargs):
         balancete = Balancete.objects.get(pk=kwargs["pk"])
-        valor = round(float(request.POST['valor']), 2)
+        valor = round(float(request.POST["valor"]), 2)
         valor = valor if valor > 0 else valor * -1
-        dados = {'nome': request.POST['nome'],
-                 'valor': valor, 
-                 'boleto': request.POST['boleto'],
-                 'balancete': balancete
-                }
+        dados = {
+            "nome": request.POST["nome"],
+            "valor": valor,
+            "boleto": request.POST["boleto"],
+            "balancete": balancete,
+        }
         try:
             Receita.objects.create(**dados)
             balancete.saldo += valor
             balancete.save()
-            return redirect('financas:index')
+            return redirect("financas:index")
         except:
-            return render(request, "financas/index.html", {"feedback": "Não foi possível criar receita!"})
+            return render(
+                request,
+                "financas/index.html",
+                {"feedback": "Não foi possível criar receita!"},
+            )
+
 
 class DespesaView(View):
     def get(self, request, *args, **kwargs):
-        balancete = Balancete.objects.get(pk=kwargs['pk'])
-        return render(request, 'financas/despesa.html', {'balancete': balancete})
+        balancete = Balancete.objects.get(pk=kwargs["pk"])
+        return render(request, "financas/despesa.html", {"balancete": balancete})
 
     def post(request, **kwargs):
         balancete = Balancete.objects.get(pk=kwargs["pk"])
-        valor = round(float(request.POST['valor']), 2)
+        valor = round(float(request.POST["valor"]), 2)
         valor = valor if valor < 0 else valor * -1
-        dados = {'nome': request.POST['nome'],
-                 'valor': valor, 
-                 'boleto': request.POST['boleto'],
-                 'balancete': balancete
-                }
+        dados = {
+            "nome": request.POST["nome"],
+            "valor": valor,
+            "boleto": request.POST["boleto"],
+            "balancete": balancete,
+        }
         try:
             Despesa.objects.create(**dados)
             balancete.saldo += valor
             balancete.save()
-            return redirect('financas:index')
+            return redirect("financas:index")
         except:
-            return render(request, "financas/index.html", {"feedback": "Não foi possível criar despesa!"})
-        
+            return render(
+                request,
+                "financas/index.html",
+                {"feedback": "Não foi possível criar despesa!"},
+            )
+
+
 class UserView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'financas/login.html')
-    
+        return render(request, "financas/login.html")
+
     def post(self, request, *args, **kwargs):
-        credenciais = {'username': request.POST['login'], 'password': request.POST['senha']}
+        credenciais = {
+            "username": request.POST["login"],
+            "password": request.POST["senha"],
+        }
         user = authenticate(request, **credenciais)
         if user:
             login(request, user)
-            return redirect('financas:index')
+            return redirect("financas:index")
         else:
-            return render(request, 'financas/login.html', {'error': f"credenciais {credenciais['login']} e {credenciais['senha']} não autenticadas, corrija os campos e tente novamente!"})
-    
+            return render(
+                request,
+                "financas/login.html",
+                {
+                    "error": f"credenciais {credenciais['login']} e {credenciais['senha']} não autenticadas, corrija os campos e tente novamente!"
+                },
+            )
+
     def logout(request):
         logout(request)
-        return redirect('financas:login')
+        return redirect("financas:login")
