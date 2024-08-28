1eb510f8cd32c5e1968260e649e64ba6590a04d7
Lipeeeeeee <favasconcelos09@gmail.com>
2024-08-28 20:25:18 +0000

    Tirando media do gitignore

--- mysite/financas/views.py
+++ mysite/financas/views.py
@@ -1,37 +1,22 @@
 from django.shortcuts import render, redirect
 from django.views import View
-<<<<<<< HEAD
-from .models import Balancete, Transacao
-=======
 from django.contrib.auth.models import User
 from .models import Balancete, Transacao, Receita, Despesa
->>>>>>> f010b2ced32f43839126dcd390b35493080313ca
 
 class BalanceteView(View):
     def get(self, request, *args, **kwargs):
         balancetes = Balancete.objects.all()
         return render(request, "financas/index.html", {"balancetes": balancetes})
-<<<<<<< HEAD
 
-    def post(self, request, *args, **kwargs):
-=======
-    
     def post(request, *args, **kwargs):
->>>>>>> f010b2ced32f43839126dcd390b35493080313ca
         nome = request.POST["nome"]
         try:
             Balancete.objects.create(nome=nome, user_id=1)
         except Exception as error:
             return render(request, "financas/index.html", {"feedback": "Não foi possível criar balancete!"})
-<<<<<<< HEAD
         return render(request, "financas/index.html", {"feedback": f"Balancete {nome} criado com sucesso!"})
 
-    def get(self, request, *args, **kwargs):
-=======
-        return redirect("financas:index")
-    
     def get_balancete(request, **kwargs):
->>>>>>> f010b2ced32f43839126dcd390b35493080313ca
         balancete = Balancete.objects.get(pk=kwargs["pk"])
         return render(request, "financas/balancete.html", {"balancete": balancete})
 
@@ -54,7 +39,7 @@ class ReceitaView(View):
         valor = round(float(request.POST['valor']), 2)
         valor = valor if valor > 0 else valor * -1
         dados = {'nome': request.POST['nome'],
-                 'valor': valor, 
+                 'valor': valor,
                  'boleto': request.POST['boleto'],
                  'balancete': balancete
                 }
@@ -76,7 +61,7 @@ class DespesaView(View):
         valor = round(float(request.POST['valor']), 2)
         valor = valor if valor < 0 else valor * -1
         dados = {'nome': request.POST['nome'],
-                 'valor': valor, 
+                 'valor': valor,
                  'boleto': request.POST['boleto'],
                  'balancete': balancete
                 }
